f392450dc360e783bcf871a1a23a849f66cd2e90
Aditi Jain <jaditi930@gmail.com>
2022-10-21 00:33:34 +0530

    Started Sign In User

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('signup_user/',views.signup_user)
 ]
--- login/views.py
+++ login/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-
+from .models import RegisterUser
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello there!")
+    return render(request,"index.html")
+
+def signup_user(request):
+    username=request.POST["name"]
+    password=request.POST["password"]
+    new_user=RegisterUser.objects.create(username=username,password=password,emailuser="aditi")
+    new_user.save()
+    return render(request,"signup_user.html",context={"name":username})
