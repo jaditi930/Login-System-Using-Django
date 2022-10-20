f7706e1bc25f929d32fa18fb89ade25a861e4465
Aditi Jain <jaditi930@gmail.com>
2022-10-21 03:01:42 +0530

    SignUp/Login Working

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,8 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.index),
-    path('signup_user/',views.signup_user)
+    path('signup_user/',views.signup_user),
+    path('logged_user/',views.log_user),
+    path('logged_user/user_authenticate/',views.authenticate_user),
+    path('logged_user/user_authenticate/logout/',views.logout_user),
 ]
--- login/views.py
+++ login/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import Http404, HttpResponse,JsonResponse
 from .models import RegisterUser
+from django.contrib.auth import authenticate,login,logout
+
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+def log_user(request):
+    return render(request,"login.html")
 
+def authenticate_user(request):
+    username=request.POST["name"]
+    password=request.POST["password"]
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request,user)
+        return render(request,"user_details.html")
+    else:
+        return HttpResponse("Not found")
+def logout_user(request):
+    logout(request)
+    return render(request,"index.html")
 def signup_user(request):
     username=request.POST["name"]
     password=request.POST["password"]
     profile_picture=request.FILES["photo"]
     new_user=RegisterUser.objects.create(username=username,password=password,profile_picture=profile_picture)
+    new_user.set_password(password)
     new_user.save()
+
     return render(request,"signup_user.html",context={"name":username})
