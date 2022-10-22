36c30949afd9f7d961b056ec39e8dd5915580fea
Aditi Jain <jaditi930@gmail.com>
2022-10-22 16:37:28 +0530

    Slight Bug Fixes

--- login/forms.py
+++ login/forms.py
@@ -1,12 +1,11 @@
-import imp
-from django.forms import ModelForm
-
+from django import forms
 from login.models import RegisterUser
 from django.forms import TextInput
-class RegisterUserForm(ModelForm):
+class RegisterUserForm(forms.ModelForm):
+    confirm_pass=forms.CharField(max_length=200, required=True,widget=TextInput(attrs={'type': 'password'}))
     class Meta:
         model=RegisterUser
-        fields=['first_name','last_name','username','password','email','role','address','state','pincode','city','profile_picture']
+        fields=['first_name','last_name','username','password','confirm_pass','email','role','address','state','pincode','city','profile_picture']
         widgets={
             'password': TextInput(attrs={'type': 'password'}),
         }
