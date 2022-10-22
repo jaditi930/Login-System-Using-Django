7f72523316314e749a37c76cd02f2997f345a4e3
Aditi Jain <jaditi930@gmail.com>
2022-10-23 04:04:52 +0530

    Finally Completed

--- login/models.py
+++ login/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 role_choices=(("Patient","Patient"),("Doctor","Doctor"))
 class RegisterUser(User):
-    profile_picture=models.ImageField(null=True)
-    address=models.CharField(max_length=200,null=True)
-    state=models.CharField(max_length=200,null=True)
-    city=models.CharField(max_length=200,null=True)
-    pincode=models.BigIntegerField(null=True)
+    profile_picture=models.ImageField(null=True,blank=True)
+    address=models.CharField(max_length=200,null=True,blank=True)
+    state=models.CharField(max_length=200,null=True,blank=True)
+    city=models.CharField(max_length=200,null=True,blank=True)
+    pincode=models.BigIntegerField(null=True,blank=True)
     role=models.CharField(max_length=200,choices=role_choices,default="Patient")
