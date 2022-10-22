c157d09fa80b83cfcfcca36151bb6067d30c83b8
Aditi Jain <jaditi930@gmail.com>
2022-10-22 10:17:12 +0530

    Bugs Fixed

--- login/views.py
+++ login/views.py
@@ -19,7 +19,7 @@ def authenticate_user(request):
         login(request,user)
         return render(request,"user_details.html",{"user":user})
     else:
-        return HttpResponse("Not found")
+        return redirect("/")
 def logout_user(request):
     logout(request)
     return redirect("/")
@@ -27,12 +27,10 @@ def signup_user(request):
     username=request.POST["name"]
     password=request.POST["password"]
     profile_picture=request.FILES["photo"]
-    exis_user=RegisterUser.objects.get(username=username)
-    print(exis_user)
-    if exis_user is None:
+    try:
         new_user=RegisterUser.objects.create(username=username,password=password,profile_picture=profile_picture)
         new_user.set_password(password)
         new_user.save()
         return render(request,"login.html")
-    else:
-        return HttpResponse("User Already Exists!Try Again!")
+    except:
+        return redirect("/signin/")
