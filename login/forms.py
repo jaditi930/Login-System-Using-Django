57d721b76a06e03d1a1d3bf5af8c50ae2f849f38
Aditi Jain <jaditi930@gmail.com>
2022-10-22 15:53:29 +0530

    Slight Bug Fixes

--- login/forms.py
+++ login/forms.py
@@ -2,7 +2,11 @@ import imp
 from django.forms import ModelForm
 
 from login.models import RegisterUser
+from django.forms import TextInput
 class RegisterUserForm(ModelForm):
     class Meta:
         model=RegisterUser
-        fields=['first_name','last_name','username','password','role','address','state','pincode','city','profile_picture']
+        fields=['first_name','last_name','username','password','email','role','address','state','pincode','city','profile_picture']
+        widgets={
+            'password': TextInput(attrs={'type': 'password'}),
+        }
--- login/models.py
+++ login/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
