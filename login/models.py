aa8aee9815976d09a22828c8e9829bf06091f45a
Aditi Jain <jaditi930@gmail.com>
2022-10-22 10:29:24 +0530

    Model image fixed

--- login/models.py
+++ login/models.py
@@ -3,4 +3,5 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class RegisterUser(User):
-    profile_picture=models.ImageField(upload_to="user_profiles",null=True)
+    profile_picture=models.ImageField(f'{id}',null=True)
+    
